ee06cf66782645aa37f891d45a417b7d33c76ac7
Julian Klotz <post@julianklotz.de>
2017-04-05 15:35:54 +0200

    Static asset serving; mako templating

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import cherrypy
+import cherrypy, os, core.mako
 
 class Tinderism(object):
     def index(self):
@@ -8,4 +8,16 @@ class Tinderism(object):
 
     index.exposed = True
 
-cherrypy.quickstart(Tinderism())
+if __name__ == '__main__':
+    static_root = os.path.dirname(os.path.abspath(__file__))
+    static_root = os.path.join( static_root, 'static')
+
+    conf = {
+        '/': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': static_root
+        }
+    }
+    print(static_root)
+
+    cherrypy.quickstart(Tinderism(), '/', conf)
