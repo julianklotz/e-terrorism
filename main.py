766aada74cc8f4ffc937abb18f393e817b2a1c38
Fabian Brosda <f.brosda@gmx.de>
2017-04-06 06:47:07 +0200

    Replaced filter with list comprehension

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ ROOT = os.path.dirname(os.path.abspath(__file__))
 class Tinderism(object):
 
     def __init__(self):
-        pass
-        #self.classifier = DecisionTreeClassifier( TD.attr_data, TD.type_data )
+        self.classifier = DecisionTreeClassifier( TD.attr_data, TD.type_data )
 
     def index(self):
         template = Template(filename='templates/index.html', output_encoding='utf-8', encoding_errors='replace', input_encoding='utf-8')
@@ -36,9 +35,8 @@ class Tinderism(object):
         # e
 
         # get input as an array with 0|1 for each attribute
-        types = self.classifier.classify( DataFrame( [[1,0,0,1,0,1,0,0]], columns=CFG.ATTRIBUTES))
+        classification_results = self.classifier.classify( DataFrame( [[1,0,0,1,0,1,0,0]], columns=CFG.ATTRIBUTES))
 
-        classification_results = [ 'BedAndBreakfast', 'Campground', 'Hotel', 'Event' ]
         for res in classification_results:
             self.resolve_generic_type( res )
 
