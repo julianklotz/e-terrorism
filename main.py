36bc338c55ad75f75a3b539b905ee70cd2ff46e7
Julian Klotz <post@julianklotz.de>
2017-04-06 11:50:07 +0200

    Populates data frame from frontend

--- main.py
+++ main.py
@@ -17,9 +17,18 @@ class Tinderism(object):
     def __init__(self):
         self.classifier = DecisionTreeClassifier( TD.attr_data, TD.type_data )
 
-    def generate(self):
+    def generate(self, adventure=-1, active=-1, family=-1, outdoor=-1, wellness=-1, couple=-1, culture=-1, lowbudget=-1):
         # get input as an array with 0|1 for each attribute
-        classification_results = self.classifier.classify( DataFrame( [[1,0,0,1,0,1,0,0]], columns=CFG.ATTRIBUTES))
+        classification_results = self.classifier.classify( DataFrame( [[
+            self.make_bool( adventure ),
+            self.make_bool( active ),
+            self.make_bool( family ),
+            self.make_bool( outdoor ),
+            self.make_bool( wellness ),
+            self.make_bool( couple ),
+            self.make_bool( culture ),
+            self.make_bool( lowbudget ),
+        ]], columns=CFG.ATTRIBUTES))
 
         results = result_factory.ResultFactory().get_results_for( classification_results )
 
@@ -28,6 +37,14 @@ class Tinderism(object):
         return results.toJSON()
 
     generate.exposed = True
+
+    def make_bool(self, val):
+        if(val == 'true'):
+            return True
+        elif(val == 'false'):
+            return False
+        else:
+            return val
 
 
 
