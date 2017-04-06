cdb9dac24cfc9b053a1f934e83e6c8d0fa135a0e
Julian Klotz <post@julianklotz.de>
2017-04-06 11:36:01 +0200

    The great rename

--- core/result_factory.py
+++ core/result_factory.py
@@ -15,9 +15,9 @@ class ResultObject():
         return "{}, {}".format(self.name, self.clazz)
 
 class ResultSet():
-    lodgings = []
-    eat_and_drink = []
-    events = []
+    GROUP_LODGINGS = []
+    GROUP_EAT_AND_DRINK = []
+    GROUP_EVENTS = []
 
 
     def toJSON(self):
@@ -35,20 +35,20 @@ class ResultFactory():
 
             if( group == GROUP_LODGINGS):
                 recommendations = self.endpoint.recommended_lodgings( cat )
-                result_set.lodgings += recommendations
+                result_set.GROUP_LODGINGS += recommendations
 
             elif( group == GROUP_EAT_AND_DRINK ):
                 print("GROUP EAT AND DRINK")
                 print("CAT: " + cat)
                 recommendations = self.endpoint.recommended_eat_and_drink( cat )
                 if(recommendations):
-                    result_set.eat_and_drink += recommendations
+                    result_set.GROUP_EAT_AND_DRINK += recommendations
 
             elif( group == GROUP_EVENTS ):
                 recommendations = self.endpoint.recommended_events( cat )
 
                 if(recommendations):
-                    result_set.events += recommendations
+                    result_set.GROUP_EVENTS += recommendations
             else:
                 print("Passing group: " + str(group))
 
