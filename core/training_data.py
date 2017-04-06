b90a684faf3742de0ca2216a94b44e8322d78393
Julian Klotz <post@julianklotz.de>
2017-04-06 11:14:40 +0200

    Different seps

--- core/training_data.py
+++ core/training_data.py
@@ -3,7 +3,7 @@
 import pandas
 
 # Attributes ( Input )
-attr_data = pandas.read_csv( 'data/training_input.csv', skipinitialspace=True )
+attr_data = pandas.read_csv( 'data/training_input.csv', skipinitialspace=True, sep=';' )
 
 # Type ( Output )
 type_data = pandas.read_csv( 'data/training_output.csv', skipinitialspace=True )
